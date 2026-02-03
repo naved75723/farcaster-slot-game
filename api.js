8a469ad059af96026808be551b9d224c511c7dbd
Mohammad Naved <82336476+naved75723@users.noreply.github.com>
2026-02-03 11:03:04 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-xport default function handler(req, res) {
+xort default function handler(req, res) {
   const symbols = ["🍒","🍋","🔔","⭐"];
   const s1 = symbols[Math.floor(Math.random()*symbols.length)];
   const s2 = symbols[Math.floor(Math.random()*symbols.length)];
