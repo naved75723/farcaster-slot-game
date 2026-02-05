595869dfc5440bb128096d65ee1d651b8db8a89c
Mohammad Naved <82336476+naved75723@users.noreply.github.com>
2026-02-05 10:46:31 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-xport default function handler(req, res) {
+export default function handler(req, res) {
   const symbols = ["🍒","🍋","🔔","⭐"];
   const s1 = symbols[Math.floor(Math.random()*symbols.length)];
   const s2 = symbols[Math.floor(Math.random()*symbols.length)];
